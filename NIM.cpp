cf54df7d43ae90022ff7fd32248df6fd2e1163be
JonHelgi <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-10-16 14:39:37 +0000

    Jon Helgi var lika her

--- NIM.cpp
+++ NIM.cpp
@@ -40,6 +40,7 @@ int main( )
 		cout << "                      2. Birta reglurnar i NIM" << endl;
 		cout << "                      3. Haetta " << endl << endl;
 		cout << "                      Veldu 1, 2 eda 3:" << endl << endl;
+		cout << "           Jon Helgi var her            "<<endl<<endl;>
 		cin >> val;
 		switch ( val )
 		{
